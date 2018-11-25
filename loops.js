f30173031e98ec190cc78bbe54e3672f6cf764da
Junyoung McArdle <juny.mcardle@gmail.com>
2018-11-25 00:58:57 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,8 @@
 for (i = 0; i < 25; i++) {
-  if i = 1
-  console.log("I am 1 strange loop")
+  function forLoop(i) {
+    if i = 1
+  console.log("I am 1 strange loop");
   else
-  console.log(`I am ${i} strange loops`)
+  console.log(`I am ${i} strange loops`);
+ }
 }
